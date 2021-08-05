11e8742ae69924e0061ffa83fb6f88f9b7b3706e
Milan <milanaiub@gmail.com>
2021-08-06 00:06:30 +0900

    Separate TabBarItem

---
+++ scai_ios_test/Scene/TabBar/TabBarItem.swift
@@ -0,0 +1,45 @@
+//
+//  TabBarItem.swift
+//  scai_ios_test
+//
+//  Created by Milan Mia on 8/6/21.
+//
+
+import UIKit
+
+enum TabBarItem: Int {
+    case camera, gallery
+
+    private func controller(with viewModel: ViewModel, navigator: Navigator) -> UIViewController {
+        if let viewController = StoryboardName.landing.viewController as? LandingViewController {
+            viewController.setup(viewModel: viewModel, navigator: navigator)
+            return viewController
+        }
+        return UIViewController()
+    }
+
+    var image: UIImage? {
+        switch self {
+        case .camera: return UIImage(systemName: "camera")
+        case .gallery: return UIImage(systemName: "list.and.film")
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .camera: return "Camera"
+        case .gallery: return "Gallery"
+        }
+    }
+
+    var tintColor: UIColor? {
+        return UIColor.orange
+    }
+
+    func getController(with viewModel: ViewModel, navigator: Navigator) -> UIViewController {
+        let vc = controller(with: viewModel, navigator: navigator)
+        let item = UITabBarItem(title: title, image: image, tag: rawValue)
+        vc.tabBarItem = item
+        return vc
+    }
+}
