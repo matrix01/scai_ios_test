212fcb57cfbbae338bd05834d8f693b58483a6a4
Milan <milanaiub@gmail.com>
2021-08-05 21:51:17 +0900

    Initial Commit

---
+++ scai_ios_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scai_ios_test
+//
+//  Created by Milan Mia on 8/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
